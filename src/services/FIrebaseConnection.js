032423bd624acc1ab828648820631c877c3949b7
Dayane Alves <99356715+dayaneAlls@users.noreply.github.com>
2024-10-27 19:03:17 -0300

    Update FIrebaseConnection.js

--- src/services/FIrebaseConnection.js
+++ src/services/FIrebaseConnection.js
@@ -1,20 +1,2 @@
-import firebase from "firebase/app";
-import { initializeApp } from "firebase/app";
-import 'firebase/database';
-import 'firebase/auth'
-
-let firebaseConfig = {
-    apiKey: "AIzaSyCRzQzsaG-2TLIwApM2RWN82oANB4vBjwg",
-    authDomain: "meuapp-edc79.firebaseapp.com",
-    databaseURL: "https://meuapp-edc79-default-rtdb.firebaseio.com",
-    projectId: "meuapp-edc79",
-    storageBucket: "meuapp-edc79.appspot.com",
-    messagingSenderId: "631671880051",
-    appId: "1:631671880051:web:f3b811dcf178e18eccad06"
-  };
-
-// Initialize Firebase
-
-if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
-
+import firebase from "firebase";
 export default firebase;
